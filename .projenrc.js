c03d8e4b6dc3bc355cc170bad1a8fa1076b8a7e6
neilkuan <guan840912@gmail.com>
2023-04-11 02:02:22 +0000

    chore: upgrade deps

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { awscdk } = require('projen');
+const { awscdk, github } = require('projen');
 const PROJECT_DESCRIPTION = 'Use AWS CDK to create budibase server';
 const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: '2.63.0',
@@ -15,18 +15,22 @@ const project = new awscdk.AwsCdkConstructLibrary({
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve'],
+      projenCredentials: github.GithubCredentials.fromPersonalAccessToken({
+        secret: 'AUTO_MACHINE_TOKEN',
+      }),
     },
   },
+
   autoApproveOptions: {
-    secret: 'GITHUB_TOKEN',
-    allowedUsernames: ['neilkuan', 'auto-machine'],
+    secret: 'PROJEN_GITHUB_TOKEN',
+    allowedUsernames: ['auto-machine', 'neilkuan'],
   },
   publishToPypi: {
     distName: 'cdk-budibase',
     module: 'cdk_budibase',
   },
-  workflowNodeVersion: '^14.17.0',
-  typescriptVersion: '3.9.10',
+  workflowNodeVersion: '^16',
+  typescriptVersion: '^4.9',
 });
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage'];
 project.gitignore.exclude(...common_exclude);
